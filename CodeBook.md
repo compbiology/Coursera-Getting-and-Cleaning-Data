82472281f15340ac903ccecef5d35021e888bef9
compbiology <compbiology@yahoo.com>
2016-03-06 02:42:27 -0500

    files for week4 assignment

---
+++ CodeBook.md
@@ -0,0 +1,46 @@
+#Codebook file to accompany the Coursera/JHU Getting and Cleaning Data class, Week 4 Assignment
+
+
+##Todd Taylor
+##2/29/16
+
+
+This file describes changes and merges made to the data files:
+
+*features.txt
+*X_test.txt
+*X_train.txt
+*y_test.txt
+*y_train.txt
+*activity_labels.txt
+*subject_test.txt
+*subject_train.txt
+
+
+taken from a dataset of smartphone accelerometer derived data intended to characterize the physical
+activity of the phone users.  Briefly, this original dataset contained ~10000 observations from 30 people,
+where each person performed six physical activities. Each observation had 561 measurements associated
+with it. The dataset is spread out among the 8 files listed above, which can be obtained at:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+There were several steps involved in creating a tidy data set for the Getting and Cleaning Data week 4 
+class assignment.
+
+*started up R
+*read in necessary R libraries, read the separate files above into separate data frames
+*the activity and subject columns were tacked onto the right side of the big 561 column test and train frames
+*the generic header was replaced with the contents of features.txt plus "activity" and "subject"
+*the resulting frame answers question1
+*I grepped out the column numbers where the column name contains ("Mean" or "mean") or "std" 
+*I concatenated and sorted these two vectors from grep 
+*I selected  only these columns from the big frame from question1" to answer question2
+*I cut the subject and activity columns off the end  of the frame from question1
+*I joined this two column frame with activity_labels
+*I cut out the activity column from the joined frame to leave a two col frame with subjects and activity labels
+*I tacked this onto the right side of the frame from question2 to answer question3
+*I replaced existing hypens  and commas with underscores using gsub to answer question4.
+*I sorted  the frame from question4  first by activity_label and then by subject using arrange to get the tidy set.
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,87 @@
+#############################################
+#  read in necessary libraries, read the separate files
+#  into separate data frames
+#############################################
+library(plyr)
+library(dplyr)
+library(data.table)
+
+features<-read.table("features.txt")
+xtest<-read.table("test/X_test.txt")
+xtrain<-read.table("train/X_train.txt")
+ytest<-read.table("test/y_test.txt")
+ytrain<-read.table("train/y_train.txt")
+activity_labels<- read.table("activity_labels.txt")
+stest<-read.table("test/subject_test.txt")
+strain<-read.table("train/subject_train.txt")
+
+#############################################
+#  tack the activity and subject columns onto the right
+#  side of the big 561 column test and train frames
+#  and replace the generic header with the contents
+#  of features.txt plus "activity" and "subject"
+#  the frame data_question1 is the answer to question1
+#############################################
+datax<-bind_cols(xtest,ytest,stest)
+datay<-bind_cols(xtrain,ytrain,strain)
+#data<-bind_rows(list(datax,datay))
+temp<-list(datax,datay)
+data_question1<-(rbindlist(temp))
+
+f<-as.character(features[,2])
+names<-c(f,"activity","subject")
+names(data_question1)<-names
+
+#############################################
+#  grep out the column numbers where the column
+#  name contains ("Mean" or "mean") or "std", then 
+#  concatenate and sort these two vectors; then 
+#  select only these columns from the big frame from 
+#  question1" to make the answer to question2
+#############################################
+mean<-grep("[M|m]ean",names)
+std<-grep("std",names)
+mean_std<-sort(c(mean,std))
+data_question2<-select(data_question1,mean_std)
+
+#############################################
+#   cut the subject and activity columns off the end  
+#   of the frame from question1; join this two column
+#   frame with activity_labels; cut out the activity 
+#   column from the joined frame to leave a two col 
+#   frame with subjects and activity labels; tack this
+#   onto the right side of the frame from question2
+#############################################
+subject_activity<-select(data_question1,-(1:561))
+names(activity_labels)<-c("activity","activity_label")
+join_data<-join(subject_activity,activity_labels,type="inner")
+join_data_trim<-select(join_data, -1)
+data_question3<-bind_cols(data_question2,join_data_trim)
+
+#############################################
+#   the variable names are almost fine as they are; they 
+#   are sufficiently descriptive now, and replacing capital
+#   letters, underscores, etc as advocated in the lecture  
+#   is silly; the whole point of capitalizing the first letter
+#   of each word in a variable name or using underscores
+#   between words is to make it easier  to see where 
+#   each word begins eliminating them makes it HARDER 
+#   to read; I only replaced existing hypens  and commas, 
+#   that might  interfere with parsing, with underscores
+#############################################
+data_question4<-data_question3
+new_names<-gsub("-|,","_",names(data_question3))
+names(data_question4)<-new_names
+
+#############################################
+#  the "average of each variable" has already been 
+#  computed; as I read the question, I don't need to use 
+#  something like ddply to compute more means, I just 
+#  need to sort  first by activity_label and then by subject, 
+#  which I have done using arrange 
+#############################################
+data_question5<-arrange(data_question4,activity_label,subject)
+
+write.table(data_question5, file = "tidy_data.csv", sep = ",")
+
+
